d11ebee049746e40acbb279f4448537eab31c0f0
Kellem Porteous <1005781@student.sae.edu.au>
2016-11-03 01:11:35 +1000

    Added a bunvh of the core mechanics of the game, current a rough look of what level 1 will be

---
+++ 16T3 GAM111.1/Assets/Player.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    private Transform myTransform;
+    public Rigidbody rb;
+
+    public float walkSpeed;
+    public float jumpForce;
+    private float distToGround;
+    private float distToWall;
+
+    public GameObject playerModel;
+    private int facingDirection = 1;
+    private Quaternion facingRotation;
+
+    public bool key = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        myTransform = this.transform;
+        rb = GetComponent<Rigidbody>();
+
+        distToGround = myTransform.GetComponent<Collider>().bounds.extents.y;
+        distToWall = myTransform.GetComponent<Collider>().bounds.extents.x;
+    }
+
+    void FixedUpdate()
+    {
+        Controls();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        DirectionFacing();
+    }
+
+    void Controls()
+    {
+        //Move Right
+        if (Input.GetKey("d"))
+        {
+            rb.MovePosition(transform.position + transform.forward * walkSpeed * Time.fixedDeltaTime);
+            facingDirection = 1;
+        }
+        //Move Left
+        else if (Input.GetKey("a"))
+        {
+            rb.MovePosition(transform.position + transform.forward * walkSpeed * Time.fixedDeltaTime);
+            facingDirection = -1;
+        }
+
+        //Jumping
+        if (Input.GetKeyDown("space") && CheckGrounded() == true)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    void DirectionFacing()
+    {
+        facingRotation = playerModel.transform.rotation;
+
+        facingRotation.eulerAngles = new Vector3(facingRotation.eulerAngles.x,
+            90 * facingDirection,
+                facingRotation.eulerAngles.z);
+
+        playerModel.transform.rotation = facingRotation;
+    }
+
+    void OnTriggerEnter(Collider otherObject)
+    {
+        if (otherObject.tag == "Spike")
+        {
+            Destroy(this.gameObject);
+        }
+
+        if (otherObject.tag == "Key")
+        {
+            key = true;
+            Destroy(otherObject.gameObject);
+        }
+
+        if (otherObject.tag == "Door")
+        {
+            if (key == false)
+            {
+                Debug.Log("Door is locked, Find the key to continue");
+                otherObject.isTrigger = false;
+            }
+
+            else if (key == true)
+            {
+                otherObject.isTrigger = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider otherObject)
+    {
+        if (otherObject.tag == "Door")
+        {
+            key = false;
+        }
+    }
+
+    public bool CheckGrounded()
+    {
+        return Physics.Raycast(myTransform.position, -Vector3.up, distToGround + 0.1f);
+    }
+}
---
+++ 16T3 GAM111.1/Assets/UI_Manager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class UI_Manager : MonoBehaviour {
+
+    public GameObject pauseMenu;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Time.timeScale == 1)
+            {
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                pauseMenu.SetActive(false);
+            }
+        }
+    }
+}
